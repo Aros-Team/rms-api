f1a8bcc82bb43ee3e498f4034d56bb1ca64617c7
Spalax-dev <jorgity345@gmail.com>
2026-03-07 03:11:28 -0500

    feat: add SecurityConfig to disable security and permit all endpoints

--- src/main/java/aros/services/rms/App.java
+++ src/main/java/aros/services/rms/App.java
@@ -3,9 +3,8 @@ package aros.services.rms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
+@SpringBootApplication
 public class App {
 
   public static void main(String[] args) {
