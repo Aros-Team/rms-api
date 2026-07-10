c86bfcba3e8538fd703f0e22c54154df84e8ae57
spalax-dev <jorgity345@gmail.com>
2026-07-09 19:16:24 -0500

    Revert "fix: PageImpl serialization warning - use PagedModel VIA_DTO"
    This reverts commit 3339ec57a3a660d72add2a38fdea5c2758a16ef2.

--- src/main/java/aros/services/rms/App.java
+++ src/main/java/aros/services/rms/App.java
@@ -6,8 +6,6 @@ import aros.services.rms.config.AppProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.data.web.config.EnableSpringDataWebSupport;
-import org.springframework.data.web.config.EnableSpringDataWebSupport.PageSerializationMode;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 /**
@@ -18,7 +16,6 @@ import org.springframework.scheduling.annotation.EnableAsync;
  */
 @SpringBootApplication
 @EnableConfigurationProperties(AppProperties.class)
-@EnableSpringDataWebSupport(pageSerializationMode = PageSerializationMode.VIA_DTO)
 @EnableAsync
 public class App {
 
